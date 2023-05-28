132daa73e6e7a03dc981d8631390792876da597f
Whale Works <oliverfpower@gmail.com>
2023-05-28 12:47:17 -0700

    redid scoring logic

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,12 @@
 //sorting out my button first
 var gameContainer = document.getElementById('theBigContainer');
 
+var header = document.getElementById('header');
+
+var quizContainer = document.getElementById('container-container');
+
+var quizQuestion = document.getElementById('quiz-question');
+
 var resultsContainer = document.getElementById('resultsContainer');
 
 var enterName = document.getElementById('enterName')
@@ -14,6 +20,10 @@ var resultsBtn = document.getElementById('resultsBtn');
 var backBtn = document.getElementById('backBtn');
 
 var options = document.getElementById('options');
+var op1 = document.getElementById('op1')
+var op2 = document.getElementById('op2')
+var op3 = document.getElementById('op3')
+var op4 = document.getElementById('op4')
 
 var quizStart = false;
 
@@ -24,67 +34,6 @@ var timer = 30;
 var interval;
 
 var points = 0;
-
-
-//gotta have a buttons section :D and theyre pretty buttons hehehe
-//starts the quiz
-//disappears buttons for questions to take place 
-startBtn.addEventListener('click', function () {
-    gameContainer.style.display = "block";
-    startBtn.style.display = 'none';
-    resultsBtn.style.display = 'none';
-    backBtn.style.display = 'none';
-    quizStart = true;
-
-    renderTimer();
-
-    interval = setInterval(function () {
-        if (!quizStart) {
-            return;
-        }
-        timer--;
-        renderTimer();
-        if (timer <= 0) {
-            clearInterval(interval);
-
-            
-            resultsBtn.style.display = 'block'
-        }
-    }, 1000);
-
-
-
-});
-
-
-
-resultsBtn.addEventListener('click', function(){
-    quizStart == false;
-    startBtn.style.display = 'none';
-    resultsBtn.style.display = 'none';
-    backBtn.style.display = 'block';
-    localStorage.getItem(points)
-})
-
-backBtn.addEventListener('click', function(){
-    startBtn.style.display = 'block';
-    resultsBtn.style.display = 'block';
-    backBtn.style.display = 'none';
-})
-
-
-
-function renderTimer() {
-    countdown.textContent = timer + " seconds left!"
-    if (timer == 1) {
-        countdown.textContent = timer + " second left!"
-    } else if (timer == 0) {
-        gameContainer.style.display = 'none'
-    }
-
-}
-
-
 
 var questions =
     [{
@@ -140,25 +89,116 @@ var questions =
     }
     ]
 
-//question and answers variables might be redundant now dont u love when u code stuff and then leave and then have code and are like what do u do do i need u
-var quest1 = document.getElementById('quiz-question')
-var quest2 = document.getElementById('quiz-question')
-var quest3 = document.getElementById('quiz-question')
-var quest4 = document.getElementById('quiz-question')
-var quest5 = document.getElementById('quiz-question')
+    var quest1 = document.getElementById('quiz-question')
+    var quest2 = document.getElementById('quiz-question')
+    var quest3 = document.getElementById('quiz-question')
+    var quest4 = document.getElementById('quiz-question')
+    var quest5 = document.getElementById('quiz-question')
+    
+//gotta have a buttons section :D and theyre pretty buttons hehehe
+//starts the quiz
+//disappears buttons for questions to take place 
+startBtn.addEventListener('click', function () {
+    gameContainer.style.display = "block";
+    startBtn.style.display = 'none';
+    resultsBtn.style.display = 'none';
+    backBtn.style.display = 'none';
+    quizStart = true;
+    timer = 30;
 
-var op1 = document.getElementById('op1')
-var op2 = document.getElementById('op2')
-var op3 = document.getElementById('op3')
-var op4 = document.getElementById('op4')
+    quizDisplay()
 
-op1.addEventListener('click', checkAnswer)
-op2.addEventListener('click', checkAnswer)
-op3.addEventListener('click', checkAnswer)
-op4.addEventListener('click', checkAnswer)
+    renderTimer();
+
+    interval = setInterval(function () {
+        if (!quizStart) {
+            return;
+        }
+        timer--;
+        renderTimer();
+        if (timer <= 0) {
+            clearInterval(interval);
+
+            
+            resultsBtn.style.display = 'block'
+        }
+    }, 1000);
+
+});
+
+resultsBtn.addEventListener('click', function(){
+    quizStart == false;
+    startBtn.style.display = 'none';
+    resultsBtn.style.display = 'none';
+    backBtn.style.display = 'block';
+    localStorage.getItem(points)
+})
+
+backBtn.addEventListener('click', function(){
+    startBtn.style.display = 'block';
+    resultsBtn.style.display = 'block';
+    backBtn.style.display = 'none';
+})
+
+function renderTimer() {
+    countdown.textContent = timer + " seconds left!"
+    if (timer == 1) {
+        countdown.textContent = timer + " second left!"
+    } else if (timer == 0) {
+        
+        quizContainer.textContent = 'you ran out of time!'
+    }
+
+}
+
+function quizDisplay() {
+    //first i want to set what questions the quiz will cycle through
+
+    if(index >= questions.length) {
+        //stop timer
+        //clear the page
+        //view scores button
+        clearInterval(interval);
+        quizContainer.style.display = 'none'
+        //quiz not displaying again after pressing start the game possibly has to do with index not be reset?
+        resultsBtn.style.display = 'block'
+
+    } else {
+        var currentQuestion = questions[index]
+        quest1.textContent = currentQuestion.question
+    //first choice
+        op1.textContent = currentQuestion.options.one
+    //second choice
+        op2.textContent = currentQuestion.options.two
+    //third choice
+        op3.textContent = currentQuestion.options.three
+    //fourth choice
+        op4.textContent = currentQuestion.options.four
+    }
+}
 
 function checkAnswer(event) {
+    var buttonClick = event.target
+    if(questions[index].correct === buttonClick.textContent){
+        points++;
+        index++;
+        clearQuestion();
+        quizDisplay();
+    } else {
+        points--;
+        index++;
+        clearQuestion();
+        quizDisplay();
+    }
+}
 
+function clearQuestion() {
+    quizQuestion.textContent = '';
+    op1.textContent = '';
+    op2.textContent = '';
+    op3.textContent = '';
+    op4.textContent = '';
+}
     //if selected correct answer +1
     //else if selected wrong answer -1
     //move up one in the array to display next question
@@ -169,74 +209,74 @@ function checkAnswer(event) {
      //whatever choice is clicked on
     //clicked option questions.options.addEventListener('click')
 
-    var currentAnswer = questions[index].correct
+    // var currentAnswer = questions[index].correct
 
-    var currentQuestion = questions[index];
+    // var currentQuestion = questions[index];
 
-    var points = 0
+    // var points = 0
 
-    if (currentQuestion == questions[0] && currentAnswer == event.target.textContent) {
-        points += 1;
-        localStorage.setItem("score", points)
-        console.log("correct answer")
-        //give +1 pt to score 
-        //repeat this format to go through all questions and correct answers in the array
-    } else if (currentQuestion == questions[1] && currentAnswer == event.target.textContent) {
-        points += 1;
-        localStorage.setItem("score", points)
-        console.log("correct answer")
-        //give +1 pt to score 
-        //repeat this format to go through all questions and correct answers in the array
-    } else if (currentQuestion == questions[2] && currentAnswer == event.target.textContent) {
-        points += 1;
-        localStorage.setItem("score", points)
-        console.log("correct answer")
-        //give +1 pt to score 
-        //repeat this format to go through all questions and correct answers in the array
-    } else if (currentQuestion == questions[3] && currentAnswer == event.target.textContent) {
-        points += 1;
-        localStorage.setItem("score", points)
-        console.log("correct answer")
-        //give +1 pt to score 
-        //repeat this format to go through all questions and correct answers in the array
-    } else if (currentQuestion == questions[4] && currentAnswer == event.target.textContent) {
-        points += 1;
-        localStorage.setItem("score", points)
-        console.log("correct answer")
-        //give +1 pt to score 
-        //repeat this format to go through all questions and correct answers in the array
-    }
+    // if (currentQuestion == questions[0] && currentAnswer == event.target.textContent) {
+    //     points += 1;
+    //     localStorage.setItem("score", points)
+    //     console.log("correct answer")
+    //     //give +1 pt to score 
+    //     //repeat this format to go through all questions and correct answers in the array
+    // } else if (currentQuestion == questions[1] && currentAnswer == event.target.textContent) {
+    //     points += 1;
+    //     localStorage.setItem("score", points)
+    //     console.log("correct answer")
+    //     //give +1 pt to score 
+    //     //repeat this format to go through all questions and correct answers in the array
+    // } else if (currentQuestion == questions[2] && currentAnswer == event.target.textContent) {
+    //     points += 1;
+    //     localStorage.setItem("score", points)
+    //     console.log("correct answer")
+    //     //give +1 pt to score 
+    //     //repeat this format to go through all questions and correct answers in the array
+    // } else if (currentQuestion == questions[3] && currentAnswer == event.target.textContent) {
+    //     points += 1;
+    //     localStorage.setItem("score", points)
+    //     console.log("correct answer")
+    //     //give +1 pt to score 
+    //     //repeat this format to go through all questions and correct answers in the array
+    // } else if (currentQuestion == questions[4] && currentAnswer == event.target.textContent) {
+    //     points += 1;
+    //     localStorage.setItem("score", points)
+    //     console.log("correct answer")
+    //     //give +1 pt to score 
+    //     //repeat this format to go through all questions and correct answers in the array
+    // }
 
-    if(currentQuestion == questions[0] && currentAnswer !== event.target.textContent) {
-        points -= 1;
-        localStorage.setItem("score", points)
-        console.log('incorrect answer')
-        //-1 point from score
-    } else if (currentQuestion == questions[1] && currentAnswer !== event.target.textContent) {
-        points -= 1;
-        localStorage.setItem("score", points)
-        console.log('incorrect answer')
+    // if(currentQuestion == questions[0] && currentAnswer !== event.target.textContent) {
+    //     points -= 1;
+    //     localStorage.setItem("score", points)
+    //     console.log('incorrect answer')
+    //     //-1 point from score
+    // } else if (currentQuestion == questions[1] && currentAnswer !== event.target.textContent) {
+    //     points -= 1;
+    //     localStorage.setItem("score", points)
+    //     console.log('incorrect answer')
+    //      //-1 point from score
+    //     //repeat this format to go through all questions and correct answers in the array
+    // } else if (currentQuestion == questions[2] && currentAnswer !== event.target.textContent) {
+    //     points -= 1;
+    //     localStorage.setItem("score", points)
+    //     console.log('incorrect answer')
+    //      //-1 point from score
+    //     //repeat this format to go through all questions and correct answers in the array
+    // } else if (currentQuestion == questions[3] && currentAnswer !== event.target.textContent) {
+    //     points -= 1;
+    //     localStorage.setItem("score", points)
+    //     console.log('incorrect answer')
+    //      //-1 point from score
+    //     //repeat this format to go through all questions and correct answers in the array
+    // } else if (currentQuestion == questions[4] && currentAnswer !== event.target.textContent) {
+    //     points -= 1;
+    //     localStorage.setItem("score", points)
+    //     console.log('incorrect answer')
          //-1 point from score
         //repeat this format to go through all questions and correct answers in the array
-    } else if (currentQuestion == questions[2] && currentAnswer !== event.target.textContent) {
-        points -= 1;
-        localStorage.setItem("score", points)
-        console.log('incorrect answer')
-         //-1 point from score
-        //repeat this format to go through all questions and correct answers in the array
-    } else if (currentQuestion == questions[3] && currentAnswer !== event.target.textContent) {
-        points -= 1;
-        localStorage.setItem("score", points)
-        console.log('incorrect answer')
-         //-1 point from score
-        //repeat this format to go through all questions and correct answers in the array
-    } else if (currentQuestion == questions[4] && currentAnswer !== event.target.textContent) {
-        points -= 1;
-        localStorage.setItem("score", points)
-        console.log('incorrect answer')
-         //-1 point from score
-        //repeat this format to go through all questions and correct answers in the array
-    }
+   //}
 
     //rotate to next question and options somehow
     //use shift? will the elements return to the array when the page refreshes?
@@ -248,26 +288,13 @@ function checkAnswer(event) {
    //once the question is answered and the score has been recorded then move to the next array item
    //cries
     
-}
-
 
 
 //the functionality of the quiz will go in this function
-function quizDisplay() {
-    //first i want to set what questions the quiz will cycle through
-    var currentQuestion = questions[index] //making this a global variable?
-    //question 1
-    quest1.textContent = currentQuestion.question
-    //first choice
-    op1.textContent = currentQuestion.options.one
-    //second choice
-    op2.textContent = currentQuestion.options.two
-    //third choice
-    op3.textContent = currentQuestion.options.three
-    //fourth choice
-    op4.textContent = currentQuestion.options.four
-
-}
 
 quizDisplay();
 
+op1.addEventListener('click', checkAnswer)
+op2.addEventListener('click', checkAnswer)
+op3.addEventListener('click', checkAnswer)
+op4.addEventListener('click', checkAnswer)
