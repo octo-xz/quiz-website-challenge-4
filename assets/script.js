f49ba43be200a6d6fd4ee6014a615922b72fab37
Whale Works <oliverfpower@gmail.com>
2023-05-29 18:05:12 -0700

    CSS BELOVED

--- assets/script.js
+++ assets/script.js
@@ -236,10 +236,9 @@ function clearQuestion() {
 }
 
 function getScore() {
-    resultsContainer.style.display = 'flex'
-    resultsContainer.style.justifyContent = 'center'
-    // document.getElementById('initId').style.display = 'block'
-    // document.getElementById('enterInit').style.display = 'block'
+    resultsContainer.style.display = 'block'
+    //document.getElementById('initId').style.display = 'block'
+    //document.getElementById('enterInit').style.display = 'block'
     // .style.display = 'block'
     var enterInitials = document.getElementById('enterInit').value
 
